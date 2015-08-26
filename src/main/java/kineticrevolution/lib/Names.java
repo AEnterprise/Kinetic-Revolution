d84506a16b61b54e0541f4c6dbefd321c100caf0
iTitus <iTitus@users.noreply.github.com>
2015-08-26 14:37:38 +0200

    Add some constants for block names and textures.

--- src/main/java/kineticrevolution/lib/Names.java
+++ src/main/java/kineticrevolution/lib/Names.java
@@ -8,4 +8,18 @@ public class Names {
 
 	}
 
+	public static class Blocks {
+
+		public static final String GENERATOR_CASING = "generatorCasing";
+		public static final String GENERATOR_INDUCTOR = "generatorInductor";
+
+	}
+
+	public static class TileEntities {
+
+		public static final String MULTIBLOCK_BASE = "tileMultiBlockTest";
+		public static final String MULTIBLOCK_GENERATOR = "tileKineticGenerator";
+
+	}
+
 }
