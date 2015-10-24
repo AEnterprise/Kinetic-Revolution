e276ab3d18d76fdf67f92b27491946553a16bd35
AEnterprise <aenterprise2@gmail.com>
2015-10-24 18:40:34 +0200

    start of the bounding boxes

--- src/main/java/kineticrevolution/duster/BlockDusterFake.java
+++ src/main/java/kineticrevolution/duster/BlockDusterFake.java
@@ -2,9 +2,12 @@ package kineticrevolution.duster;
 
 import kineticrevolution.blocks.BlockBase;
 import kineticrevolution.util.Location;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -14,6 +17,24 @@ import java.util.Random;
  * Created by AEnterprise
  */
 public class BlockDusterFake extends BlockBase {
+	private static AxisAlignedBB[] BOXES = {
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+			AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1)
+	};
 
 
 	public BlockDusterFake() {
@@ -33,6 +54,23 @@ public class BlockDusterFake extends BlockBase {
 		location.z *= -1;
 		location.offset(x, y, z);
 		world.getBlock(location.x, location.y, location.z).onBlockDestroyedByPlayer(world, location.x, location.y, location.z, meta);
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
+		AxisAlignedBB aabb = BOXES[world.getBlockMetadata(x, y, z)];
+		setBlockBounds((float) aabb.minX, (float) aabb.minY, (float) aabb.minZ, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.maxZ);
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		setBlockBoundsBasedOnState(world, x, y, z);
+		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
+	}
+
+	@Override
+	public void setBlockBoundsForItemRender() {
+		setBlockBounds(0, 0, 0, 1, 1, 1);
 	}
 
 	@Override
@@ -58,5 +96,27 @@ public class BlockDusterFake extends BlockBase {
 	@Override
 	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
 		return new ArrayList<ItemStack>();
+	}
+
+	@Override
+	public void onFallenUpon(World world, int x, int y, int z, Entity entity, float distance) {
+		BOXES = new AxisAlignedBB[]{
+				AxisAlignedBB.getBoundingBox(0.65, 0, 0.65, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0.65, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0.65, 0, 0, 1, 1, 0.45),
+				AxisAlignedBB.getBoundingBox(0, 0, 0.65, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 0.45),
+				AxisAlignedBB.getBoundingBox(0, 0, 0.65, 0.45, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 0.45, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 0.45, 1, 0.45),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1),
+				AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1)
+		};
 	}
 }
